77e08299499eff205bd0ff70862e61e5935309ae
Alson Zhang <12924991@student.uts.edu.au>
2019-05-22 14:12:47 +1000

    Initial Commit

---
+++ Are-You-Happy?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Are-You-Happy?
+//
+//  Created by Alson Yupeng Zhang on 22/5/19.
+//  Copyright © 2019 Group-182. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
