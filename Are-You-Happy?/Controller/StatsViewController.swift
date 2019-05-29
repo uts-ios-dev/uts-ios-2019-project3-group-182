7961a1885d7a593cb2c60847297043c4ded1cb7b
Will <post4wjm@gmail.com>
2019-05-29 12:09:49 +1000

    Created StatsViewController

---
+++ Are-You-Happy?/Controller/StatsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StatsViewController.swift
+//  Are-You-Happy?
+//
+//  Created by William Mitchell on 29/5/19.
+//  Copyright © 2019 Group-182. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+    
+    // Variables
+    var rating: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
+
