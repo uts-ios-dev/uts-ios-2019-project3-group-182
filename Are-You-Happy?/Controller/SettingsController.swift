34dee741993ac32abdea82eedf0cf7e6fb9ad21c
Alson Zhang <12924991@student.uts.edu.au>
2019-06-02 20:26:42 +1000

    setup settings page

---
+++ Are-You-Happy?/Controller/SettingsController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsController.swift
+//  Are-You-Happy?
+//
+//  Created by ALson Zhang on 2/6/19.
+//  Copyright © 2019 Group-182. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO: setup activities
+        // TODO: send data to json
+    }
+}
--- Are-You-Happy?/Model/Activity.swift
+++ Are-You-Happy?/Model/Activity.swift
@@ -2,8 +2,13 @@ import Foundation
 
 class Activity {
     let name: String
+    let activte: Bool
+    let image: String
+
     
-    init(name: String) {
+    init(name: String, active: Bool, image: String) {
         self.name = name
+        self.activte = active
+        self.image = image
     }
 }
