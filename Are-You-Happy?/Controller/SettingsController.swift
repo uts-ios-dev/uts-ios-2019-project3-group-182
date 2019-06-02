712a7b084f05d0cdb935b03bde0376790c285383
Alson Zhang <12924991@student.uts.edu.au>
2019-06-02 23:10:49 +1000

    added to controller

--- Are-You-Happy?/Controller/SettingsController.swift
+++ Are-You-Happy?/Controller/SettingsController.swift
@@ -8,11 +8,48 @@
 
 import UIKit
 
-class SettingsController: UIViewController {
+class SettingsController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+
+    @IBOutlet weak var tempColor: UILabel!
+    @IBOutlet weak var colourPicker: UIPickerView!
+    @IBOutlet weak var imagePicker: UIPickerView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var colorOptions = ["red", "blue", "green", "brown", "yellow", "magenta", "pink", "cyan", "orange"]
+    var imageOptions = [""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: setup activities
         // TODO: send data to json
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if(pickerView.tag == 1) {
+            return imageOptions.count
+        } else {
+            return colorOptions.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if(pickerView.tag == 1) {
+            return imageOptions[row]
+        } else {
+            return colorOptions[row]
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if(pickerView.tag == 1) {
+            //change image view
+        } else {
+            // change colour of image? or something
+        }
+    }
+    
 }
