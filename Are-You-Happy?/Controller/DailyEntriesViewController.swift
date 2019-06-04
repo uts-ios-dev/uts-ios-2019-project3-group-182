c1bcbdf3ed02f30071bd7d0f43ee5877bc1ab8f0
Alson Zhang <12924991@student.uts.edu.au>
2019-06-04 16:22:32 +1000

    replace scrollview with tableview

--- Are-You-Happy?/Controller/DailyEntriesViewController.swift
+++ Are-You-Happy?/Controller/DailyEntriesViewController.swift
@@ -10,33 +10,35 @@ import Foundation
 import UIKit
 
 class DailyEntriesViewController: UIViewController {
-    @IBOutlet weak var dailyEntriesScrollView: UIScrollView!
+    //@IBOutlet weak var dailyEntriesScrollView: UIScrollView!
+    @IBOutlet weak var dailyEntriesTable: UITableView!
+    
     
     override func viewDidLoad() {
         
         //super.viewDidLoad()
-        let storage = Storage()
-        var dailyEntries: [DailyEntry] = []
-        do {
-            dailyEntries = try storage.loadDailyEntries()
-        } catch {}
-        for i in 0..<dailyEntries.count {
-            let frame = CGRect(x: 50 , y: 50 + i*50, width: 200, height: 50)
-            let date = dailyEntries[i].date
-            //let rating = dailyEntries[i].rating
-            //let activityNames = dailyEntries[i].activityNames
-
-
-            let dateLabel = UILabel(frame: frame)
-            dateLabel.text = date
-
-            self.dailyEntriesScrollView.addSubview(dateLabel)
-
-        }
+//        let storage = Storage()
+//        var dailyEntries: [DailyEntry] = []
+//        do {
+//            dailyEntries = try storage.loadDailyEntries()
+//        } catch {}
+//        for i in 0..<dailyEntries.count {
+//            let frame = CGRect(x: 50 , y: 50 + i*50, width: 200, height: 50)
+//            let date = dailyEntries[i].date
+//            //let rating = dailyEntries[i].rating
+//            //let activityNames = dailyEntries[i].activityNames
+//
+//
+//            let dateLabel = UILabel(frame: frame)
+//            dateLabel.text = date
+//
+//            self.dailyEntriesScrollView.addSubview(dateLabel)
+//
+//        }
 //        let frame = CGRect(x: 50 , y: 50 + 50, width: 200, height: 50)
 //        let label = UILabel(frame: frame)
 //        label.text = "testing"
-//        dailyEntriesScrollView.addSubview(label)
+//        dailyEntries.addSubview(label)
 
         
     }
