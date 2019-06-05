4000bb2e8a79a1338a595f997dca6dc66f8e99ec
Alson Zhang <12924991@student.uts.edu.au>
2019-06-05 11:36:37 +1000

    comments

--- Are-You-Happy?/Controller/ViewController.swift
+++ Are-You-Happy?/Controller/ViewController.swift
@@ -9,32 +9,32 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    //TODO: Rename function names
+    // Rating of 5 is sent to Rating function
     @IBAction func fiveButtonPressed(_ sender: Any) {
         setRatingAndGoToActivities(5)
     }
-    
+    // Rating of 4 is sent to Rating function
     @IBAction func fourPressed(_ sender: Any) {
         setRatingAndGoToActivities(4)
     }
-    
+    // Rating of 3 is sent to Rating function
     @IBAction func threePressed(_ sender: Any) {
         setRatingAndGoToActivities(3)
     }
-    
+    // Rating of 2 is sent to Rating function
     @IBAction func twoPressed(_ sender: Any) {
         setRatingAndGoToActivities(2)
     }
-    
+    // Rating of 1 is sent to Rating function
     @IBAction func onePressed(_ sender: Any) {
         setRatingAndGoToActivities(1)
     }
-    
+    // set Rating
     func setRatingAndGoToActivities(_ rating: Int) {
         self.rating = rating
         performSegue(withIdentifier: "ActivitesViewSegue", sender: nil)
     }
-    
+    // set ActivitiesViewController rating to current rating
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "ActivitesViewSegue"){
             let activitiesVC = segue.destination as! ActivitiesViewController
